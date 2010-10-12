3e63abb46dcd5d885b15093d70b878f430b889ae
Andrew Miller <ak.miller@auckland.ac.nz>
2010-10-12 20:57:16 +1300

    Added a script to build external libraries

--- other-projects/build-all.sh
+++ other-projects/build-all.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-cd levmar-2.5 && make liblevmar.a && cd ../sundials-2.4.0 && ./configure && make
+cd levmar-2.5 && make liblevmar.a && cd ../sundials-2.4.0 && ./configure --prefix=/usr && make
+cd ..
+sudo cp levmar-2.5/liblevmar.a /usr/lib
+sudo cp levmar-2.5/*.h /usr/include
+cd sundials-2.4.0
+sudo make install
